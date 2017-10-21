f9576407c5b00533327717ef1628e3137a8cf3b1
reshnaz <31286438+reshnaz@users.noreply.github.com>
2017-10-21 14:25:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// makes sure the whole page is ready
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); // This statement will enable all popovers in the document.
+});
